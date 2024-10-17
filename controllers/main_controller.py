833bc596b4f0c8b59adca8c3d20757cab83dfa94
Adel <kanso@onsite.us>
2024-10-18 01:06:35 +0200

    changes

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -4,9 +4,6 @@ from views.objects_detection_view import ObjectsDetectionView
 class MainController:
     def __init__(self, main_view):
         self.main_view = main_view
-        
-        
-        # Configure buttons to navigate to different views
         self.main_view.face_button.configure(command=self.show_face_detection)
         self.main_view.general_button.configure(command=self.show_objects_detection)
 
@@ -16,9 +13,7 @@ class MainController:
         self.face_detection_view.pack(fill='both', expand=True)
 
     def show_objects_detection(self):
-        # Initialize the face and objects detection views
         self.objects_detection_view = ObjectsDetectionView(self.main_view.master, self.main_view)
-        # Check if the objects detection view needs to be reset
-        self.main_view.frame.pack_forget()  # Hide the main frame
-        if not self.objects_detection_view.winfo_ismapped():  # Only pack if it's not already visible
-            self.objects_detection_view.pack(fill='both', expand=True)  # Show objects detection view
+        self.main_view.frame.pack_forget()
+        if not self.objects_detection_view.winfo_ismapped():
+            self.objects_detection_view.pack(fill='both', expand=True)
